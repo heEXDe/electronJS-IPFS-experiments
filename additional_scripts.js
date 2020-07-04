8a4a4a2a619a4687627a7513f33d19e48f97c1fe
heEXDe <61631314+heEXDe@users.noreply.github.com>
2020-07-05 00:53:15 +0200

    Update additional_scripts.js

--- additional_scripts.js
+++ additional_scripts.js
@@ -1,58 +1,53 @@
-// create div
-var div_buttons = document.createElement("div");
-var div_1 = document.createElement("div");
-var div_2 = document.createElement("div");
-var div_3 = document.createElement("div");
-var div_4 = document.createElement("div");
-// append div to start_page document
-document.body.appendChild(div_buttons);
-div_buttons.appendChild(div_1);
-div_buttons.appendChild(div_2);
-div_buttons.appendChild(div_3);
-div_buttons.appendChild(div_4);
-// center div_buttons
-div_buttons.style.alignSelf = "center";
-div_1.style.alignSelf = "center";
-div_2.style.alignSelf = "center";
-div_3.style.alignSelf = "center";
-div_4.style.alignSelf = "center";
+
+// get two first buttons
+var btn1 = document.getElementById("btn_1");
+var btn2 = document.getElementById("btn_2");
+
 // create buttons
-var btn1 = document.createElement("button");
-var btn2 = document.createElement("button");
 var btn_ch_f = document.createElement("button");
 var btn_ipfs = document.createElement("button");
 var btn_h1 = document.createElement("button");
 var btn_h2 = document.createElement("button");
+
 // set text for buttons
-btn1.innerHTML = "To IPFS";
-btn2.innerHTML = "From IPFS";
 btn_ch_f.innerHTML = "Choose file";
 btn_ipfs.innerHTML = "IPFS";
 btn_h1.innerHTML = "Home";
 btn_h2.innerHTML = "Home";
+
 // set id for buttons
-btn1.setAttribute("id", "btn1");
-btn2.setAttribute("id", "btn2");
 btn_ch_f.setAttribute("id", "btn_ch_f");
 btn_ipfs.setAttribute("id", "btn_ipfs");
 btn_h1.setAttribute("id", "btn_h1");
 btn_h2.setAttribute("id", "btn_h2");
+
 // set onclick events for buttons
-btn1.onclick = function(){add_file_page()};
-btn2.onclick = function(){download_file_page()};
 btn_ch_f.onclick = function(){choose_file()};
 btn_ipfs.onclick = function(){ipfs_add()};
 btn_h1.onclick = function(){to_home_from_add()};
 btn_h2.onclick = function(){to_home_from_download()};
-// append buttons for start_page
-div_1.appendChild(btn1);
-div_2.appendChild(btn2);
 
 function choose_file() {
-    /* document.getElementById('display_clicks').innerHTML = "You've clicked me!"; */
-    const { dialog } = require('electron').remote;
-    dialog.showOpenDialog({title: 'choose a file'});
-
+  //require file system module to handle files
+  var fs = require('fs');
+  // options for dialog box
+  let options = {
+    title: 'choose a file',
+    properties: ['openFile','multiSelections']
+  };
+  // require module for futher access to the dialog box
+  const { dialog } = require('electron').remote;
+  // open dialog box
+  var file = dialog.showOpenDialog(options);
+  // what to do with the selected file/files
+  file.then(result => {
+    console.log(result.filePaths[0]);
+    var file_path = result.filePaths[0];
+    fs.readFile(file_path, 'utf-8', (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  }).catch(err => {console.log(err);});
 }
 
 function add_file_page(){
